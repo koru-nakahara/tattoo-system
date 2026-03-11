ab554ff45f5480443cd76ad1ac0e24e2a1f9d0e3
tattoo-system <fanduan2017@gmail.com>
2026-03-11 17:38:20 +0900

    変更

---
+++ src/test/java/com/example/tattoo/TattooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tattoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TattooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
